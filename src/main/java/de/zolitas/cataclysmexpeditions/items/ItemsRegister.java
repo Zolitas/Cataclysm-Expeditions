5cdd4006e2a7528acefae998c9b30233b271dbb2
Zolitas <49311312+Zolitas@users.noreply.github.com>
2026-01-07 20:10:31 +0100

    Moved Expedition Anchor item registration logic from `BlocksRegister` to `ItemsRegister`.

--- src/main/java/de/zolitas/cataclysmexpeditions/items/ItemsRegister.java
+++ src/main/java/de/zolitas/cataclysmexpeditions/items/ItemsRegister.java
@@ -1,8 +1,12 @@
 package de.zolitas.cataclysmexpeditions.items;
 
 import de.zolitas.cataclysmexpeditions.CataclysmExpeditions;
+import de.zolitas.cataclysmexpeditions.blocks.BlocksRegister;
+import net.minecraft.world.item.BlockItem;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ItemsRegister {
   public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(CataclysmExpeditions.MODID);
+  public static final DeferredItem<BlockItem> EXPEDITION_ANCHOR_ITEM = ITEMS.registerSimpleBlockItem(BlocksRegister.EXPEDITION_ANCHOR_BLOCK);
 }
